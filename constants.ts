33e1e89054f9dca57bafa2e93765340a26a48991
Seybel <seyibello31@gmail.com>
2024-01-08 23:17:44 +0100

    updated

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
 export const transactionViewOptions = ["Yearly", "Monthly", "Daily"];
+export const categories = ['Food', 'Housing', 'Car', 'General Subscription', 'Electricity', 'Gym', 'Entertainment', 'Other'];
+export const types = ['Income', 'Expense', 'Saving', 'Investment'];
   
   
